1f45b6d03bc875f031fb41c3a14b10b8cf30f33a
HJD <hjdhjd@users.noreply.github.com>
2020-07-19 22:31:29 -0500

    Add more commentary.

--- src/settings.ts
+++ src/settings.ts
@@ -2,6 +2,11 @@
  *
  * settings.ts: Settings and constants for homebridge-myq2.
  */
+// The name of our plugin.
 export const PLUGIN_NAME = "homebridge-myq2";
+
+// The platform the plugin creates.
 export const PLATFORM_NAME = "myQ";
+
+// Since HomeKit doesn't give us a value for an obstructed state, we use this instead.
 export const MYQOBSTRUCTED = 8675309;
