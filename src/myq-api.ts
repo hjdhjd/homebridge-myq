b36ad803e771b37bd0bcd342e3adadabf9b1c4c5
HJD <hjdhjd@users.noreply.github.com>
2020-07-25 05:14:02 -0500

    A more complete view of the myQ API JSONs.

--- src/myq-api.ts
+++ src/myq-api.ts
@@ -14,27 +14,135 @@ import {
   MYQ_API_VERSION_MINOR
 } from "./settings";
 
-// An incomplete description of the myQ device JSON, but enough for our purposes.
-export interface myQDevice {
-  readonly device_family: string,
-  readonly device_platform: string,
-  readonly device_type: string,
-  readonly name: string,
-  readonly parent_device_id: string,
-  readonly serial_number: string,
-  readonly state: {
-    readonly door_state: string,
-    readonly dps_low_battery_mode: boolean,
-    readonly gdo_lock_connected: boolean
-    readonly online: boolean,
-    readonly firmware_version: string
+// A semi-complete description of the myQ account JSON.
+// This is currently unused and documented here primarily for reference.
+interface myQAccountInterface {
+  Admin: boolean,
+  Account: {
+    href: string,
+    AccessGroups: {
+      href: string
+    },
+    AccessSchedules: {
+      href: string
+    },
+    Address: {
+      AddressLine1: string,
+      AddressLine2: string,
+      City: string,
+      PostalCode: string,
+      Country: {
+        href: string,
+        Code: string,
+        IsEEACountry: boolean
+      }
+    },
+    ContactName: string,
+    Devices: {
+      href: string
+    },
+    DirectoryCodeLength: number,
+    Email: string,
+    Id: string,
+    Name: string,
+    Phone: string,
+    Roles: {
+      href: string
+    },
+    TimeZone: string,
+    Users: {
+      href: string
+    },
+    UserAllowance: number,
+    Zones: {
+      href: string
+    }
+  },
+  AnalyticsId: string,
+  CultureCode: string,
+  DiagnosticDataOptIn: boolean,
+  Email: string,
+  FirstName: string,
+  LastName: string,
+  Address: {
+    AddressLine1: string,
+    AddressLine2: string,
+    City: string,
+    PostalCode: string,
+    Country: {
+      href: string,
+      Code: string,
+      IsEEACountry: boolean
+    }
+  },
+  MailingListOptIn: boolean,
+  Phone: string,
+  RequestAccountLinkInfo: boolean,
+  TimeZone: { Id: string, Name: string },
+  Users: { href: string },
+  UserId: string,
+  UserName: string
+}
+
+// A semi-complete description of the myQ device JSON.
+interface myQDeviceInterface {
+  created_date: string,
+  device_family: string,
+  device_platform: string,
+  device_type: string,
+  href: string,
+  name: string,
+  parent_device?: string,
+  parent_device_id?: string,
+  serial_number: string,
+  state: {
+    attached_work_light_error_present: boolean,
+    aux_relay_behavior: string,
+    aux_relay_delay: string,
+    close: string,
+    command_channel_report_status: boolean,
+    control_from_browser: boolean,
+    door_ajar_interval: string,
+    door_state: string,
+    dps_low_battery_mode?: boolean,
+    firmware_version?: string,
+    gdo_lock_connected: boolean,
+    homekit_capable?: boolean,
+    homekit_enabled?: boolean,
+    invalid_credential_window: string,
+    invalid_shutout_period: string,
+    is_unattended_close_allowed: boolean,
+    is_unattended_open_allowed: boolean,
+    last_status: string,
+    last_update: string,
+    learn?: string,
+    learn_mode?: boolean,
+    max_invalid_attempts: number,
+    online: boolean,
+    open: string,
+    passthrough_interval: string,
+    pending_bootload_abandoned?: boolean,
+    physical_devices?: [],
+    report_ajar: boolean,
+    report_forced: boolean,
+    rex_fires_door: boolean,
+    updated_date?: string,
+    use_aux_relay: boolean
   }
 }
 
-export interface myQHwInfo {
-  readonly product: string,
-  readonly brand: string
+// Hardware device information reference.
+interface myQHwInfoInterface {
+  product: string,
+  brand: string
 }
+
+// We use types instead of interfaces here because we can more easily set the entire thing as readonly.
+// Unfortunately, interfaces can't be quickly set as readonly in Typescript without marking each and
+// every property as readonly along the way.
+type myQAccount = Readonly<myQAccountInterface>;
+export type myQDevice = Readonly<myQDeviceInterface>;
+export type myQHwInfo = Readonly<myQHwInfoInterface>;
 
 let debugMode = false;
 
