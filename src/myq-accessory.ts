ab6e11b57fc4d810a34a91f8f6809a33a8bddd2d
HJD <hjdhjd@users.noreply.github.com>
2020-09-19 13:57:00 -0500

    Move common functions into the accessory base class.

--- src/myq-accessory.ts
+++ src/myq-accessory.ts
@@ -6,7 +6,7 @@ import { API, HAP, Logging, PlatformAccessory } from "homebridge";
 
 import { myQApi } from "./myq-api";
 import { myQPlatform } from "./myq-platform";
-import { myQOptions } from "./myq-types";
+import { myQDevice, myQOptions } from "./myq-types";
 
 export abstract class myQAccessory {
   protected readonly accessory: PlatformAccessory;
@@ -38,4 +38,74 @@ export abstract class myQAccessory {
   // few seconds to refresh the accessory state based on the latest information from the
   // myQ API.
   abstract updateState(): boolean;
+
+  // Execute myQ commands.
+  protected async command(myQCommand: string): Promise<boolean> {
+
+    const device = this.accessory.context.device as myQDevice;
+
+    if(!device) {
+      this.log("%s: Can't find the associated device in the myQ API.", this.name());
+      return false;
+    }
+
+    // Execute the command.
+    await this.myQ.execute(device.serial_number, myQCommand);
+
+    // Increase the frequency of our polling for state updates to catch any updates from myQ.
+    // This will trigger polling at activeRefreshInterval until activeRefreshDuration is hit. If you
+    // query the myQ API too quickly, the API won't have had a chance to begin executing our command.
+    this.platform.pollOptions.count = 0;
+    this.platform.poll(this.config.refreshInterval * -1);
+
+    return true;
+
+  }
+
+  // Configure the device information for HomeKit.
+  protected configureInfo(): boolean {
+
+    const device = this.accessory.context.device as myQDevice;
+
+    // Set the firmware revision for this device.
+    // Fun fact: This firmware information is stored on the gateway not the device.
+    const gwParent = this.myQ.Devices.find(x => x.serial_number === device.parent_device_id);
+    let gwBrand = "Liftmaster";
+    let gwProduct = "myQ";
+
+    if(gwParent?.state?.firmware_version) {
+      const gwInfo = this.myQ.getHwInfo(gwParent.serial_number);
+
+      this.accessory
+        .getService(this.hap.Service.AccessoryInformation)
+        ?.getCharacteristic(this.hap.Characteristic.FirmwareRevision).updateValue(gwParent.state.firmware_version);
+
+      // If we're able to lookup hardware information, use it. getHwInfo returns an object containing
+      // device type and brand information.
+      gwProduct = gwInfo?.product;
+      gwBrand = gwInfo?.brand;
+    }
+
+    // Update the manufacturer information for this device.
+    this.accessory
+      .getService(this.hap.Service.AccessoryInformation)
+      ?.getCharacteristic(this.hap.Characteristic.Manufacturer).updateValue(gwBrand);
+
+    // Update the model information for this device.
+    this.accessory
+      .getService(this.hap.Service.AccessoryInformation)
+      ?.getCharacteristic(this.hap.Characteristic.Model).updateValue(gwProduct);
+
+    // Update the serial number for this device.
+    this.accessory
+      .getService(this.hap.Service.AccessoryInformation)
+      ?.getCharacteristic(this.hap.Characteristic.SerialNumber).updateValue(device.serial_number);
+
+    return true;
+  }
+
+  // Name utility function.
+  public name(): string {
+    return this.accessory.displayName ?? (this.accessory.context.device as myQDevice).name;
+  }
 }
