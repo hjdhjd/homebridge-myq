bcdc588c280ef76e45f9f47e77ebf9227f347bf1
HJD <hjdhjd@users.noreply.github.com>
2021-09-18 13:30:10 -0500

    New file to contain homebridge-myq plugin configuration options.

---
+++ src/myq-config.ts
@@ -0,0 +1,24 @@
+/* Copyright(C) 2017-2021, HJD (https://github.com/hjdhjd). All rights reserved.
+ *
+ * myq-config.ts: Configuration options for the myQ plugin.
+ */
+
+// Plugin configuration options.
+export interface myQOptionsInterface {
+
+  activeRefreshDuration: number,
+  activeRefreshInterval: number,
+  debug: boolean,
+  email: string,
+  mqttTopic: string,
+  mqttUrl: string,
+  name: string,
+  options: string[],
+  password: string,
+  refreshInterval: number
+}
+
+// We use types instead of interfaces here because we can more easily set the entire thing as readonly.
+// Unfortunately, interfaces can't be quickly set as readonly in TypeScript without marking each and
+// every property as readonly along the way.
+export type myQOptions = Readonly<myQOptionsInterface>;
